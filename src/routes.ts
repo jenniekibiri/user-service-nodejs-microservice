c315f286a20ee3f93f2e77c5ef95ebf250bbe376
jenniekibiri <josphatje@gmail.com>
2023-07-02 18:21:05 +0300

    get all users

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { Login, Register,AuthenticatedUser,Logout, UpdatePassword,UpdateInfo } from "./controller/auth.controller";
+import {
+  Login,
+  Register,
+  AuthenticatedUser,
+  Logout,
+  UpdatePassword,
+  UpdateInfo,
+} from "./controller/auth.controller";
 import { AuthMiddleware } from "./middleware/auth.middleware";
+import { Users } from "./controller/user.controller";
 
 export const routes = (router: Router) => {
   router.post("/api/register", Register);
   router.post("/api/login", Login);
-  router.get('/api/user', AuthMiddleware, AuthenticatedUser);
+  router.get("/api/user", AuthMiddleware, AuthenticatedUser);
   router.post("/api/logout", AuthMiddleware, Logout);
-  router.put("api/users/info", AuthMiddleware, UpdateInfo );
-  router.put("api/users/password", AuthMiddleware, UpdatePassword );
-
-
+  router.put("api/users/info", AuthMiddleware, UpdateInfo);
+  router.put("api/users/password", AuthMiddleware, UpdatePassword);
+  router.get("/api/users", Users);
 };
